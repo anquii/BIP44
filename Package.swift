ff4fbce0e8e074701f408662c2f1758f6b121a52
anquii <50277099+anquii@users.noreply.github.com>
2022-03-24 18:50:49 +0000

    Added Package.swift

---
+++ Package.swift
@@ -0,0 +1,33 @@
+// swift-tools-version:5.5
+
+import PackageDescription
+
+let package = Package(
+    name: "BIP44",
+    platforms: [
+        .macOS(.v10_15),
+        .iOS(.v13)
+    ],
+    products: [
+        .library(
+            name: "BIP44",
+            targets: ["BIP44"]
+        )
+    ],
+    dependencies: [
+        .package(
+            url: "https://github.com/anquii/BIP32.git",
+            .upToNextMajor(from: "1.0.0")
+        )
+    ],
+    targets: [
+        .target(
+            name: "BIP44",
+            dependencies: ["BIP32"]
+        ),
+        .testTarget(
+            name: "BIP44Tests",
+            dependencies: ["BIP44"]
+        )
+    ]
+)
