530bd1e6ad0c2ab366b9594f93cd4ef922828fc9
anquii <50277099+anquii@users.noreply.github.com>
2022-03-31 15:31:27 +0100

    Updated Package

--- Package.swift
+++ Package.swift
@@ -17,7 +17,7 @@ let package = Package(
     dependencies: [
         .package(
             url: "https://github.com/anquii/BIP32.git",
-            .upToNextMajor(from: "1.0.0")
+            .upToNextMajor(from: "1.1.0")
         )
     ],
     targets: [
