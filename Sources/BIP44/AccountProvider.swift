049aa2950c72028b13b100725f757b4c43764d12
anquii <50277099+anquii@users.noreply.github.com>
2022-04-11 17:33:34 +0100

    Updated AccountProvider

--- Sources/BIP44/AccountProvider.swift
+++ Sources/BIP44/AccountProvider.swift
@@ -2,29 +2,32 @@ import Foundation
 import BIP32
 
 public protocol AccountProviding {
-    func account(name: String, index: UInt32) throws -> Account
+    func account(configuration: AccountConfiguration) throws -> Account
 }
 
 public struct AccountProvider {
+    private static let depth = UInt8(3)
     private static let hardenedPurposeIndex = UInt32(0x8000002C)
 
     private let coinType: CoinType
-    private let privateMasterKeyDerivator: PrivateMasterKeyDerivating
     private let privateChildKeyDerivator: PrivateChildKeyDerivating
     private let keyIndexHardener: KeyIndexHardening
+    private let keySerializer: KeySerializing
     private let privateCoinTypeChildKey: ExtendedKeyable
+    private let publicCoinTypeChildKeyFingerprint: UInt32
 
     public init(
         seed: Data,
         coinType: CoinType,
         privateMasterKeyDerivator: PrivateMasterKeyDerivating = PrivateMasterKeyDerivator(),
         privateChildKeyDerivator: PrivateChildKeyDerivating = PrivateChildKeyDerivator(),
-        keyIndexHardener: KeyIndexHardening = KeyIndexHardener()
+        publicChildKeyDerivator: PublicChildKeyDerivating = PublicChildKeyDerivator(),
+        keyFingerprintDerivator: KeyFingerprintDerivating = KeyFingerprintDerivator(),
+        keyIndexHardener: KeyIndexHardening = KeyIndexHardener(),
+        keySerializer: KeySerializing = KeySerializer()
     ) throws {
         do {
-            let privateMasterKey = try privateMasterKeyDerivator.privateKey(
-                seed: seed
-            )
+            let privateMasterKey = try privateMasterKeyDerivator.privateKey(seed: seed)
             let privatePurposeChildKey = try privateChildKeyDerivator.privateKey(
                 privateParentKey: privateMasterKey,
                 index: Self.hardenedPurposeIndex
@@ -33,28 +36,42 @@ public struct AccountProvider {
                 privateParentKey: privatePurposeChildKey,
                 index: try keyIndexHardener.hardenedIndex(normalIndex: coinType.index)
             )
+            let publicCoinTypeChildKey = try publicChildKeyDerivator.publicKey(privateKey: privateCoinTypeChildKey)
+            publicCoinTypeChildKeyFingerprint = keyFingerprintDerivator.fingerprint(publicKey: publicCoinTypeChildKey.key)
         } catch {
             throw AccountProviderError.invalidInput
         }
         self.coinType = coinType
-        self.privateMasterKeyDerivator = privateMasterKeyDerivator
         self.privateChildKeyDerivator = privateChildKeyDerivator
         self.keyIndexHardener = keyIndexHardener
+        self.keySerializer = keySerializer
     }
 }
 
 // MARK: - AccountProviding
 extension AccountProvider: AccountProviding {
-    public func account(name: String, index: UInt32) throws -> Account {
+    public func account(configuration: AccountConfiguration) throws -> Account {
         do {
+            let hardenedIndex = try keyIndexHardener.hardenedIndex(normalIndex: configuration.index)
             let privateAccountChildKey = try privateChildKeyDerivator.privateKey(
                 privateParentKey: privateCoinTypeChildKey,
-                index: try keyIndexHardener.hardenedIndex(normalIndex: index)
+                index: hardenedIndex
+            )
+            let privateAccountChildKeyAttributes = ChildKeyAttributes(
+                accessControl: .`private`,
+                version: configuration.version,
+                depth: Self.depth,
+                parentKeyFingerprint: publicCoinTypeChildKeyFingerprint,
+                index: hardenedIndex
+            )
+            let privateAccountSerializedChildKey = try keySerializer.serializedKey(
+                extendedKey: privateAccountChildKey,
+                attributes: privateAccountChildKeyAttributes
             )
             return Account(
-                name: name,
+                name: configuration.name,
                 coinType: coinType,
-                extendedKey: privateAccountChildKey
+                serializedKey: privateAccountSerializedChildKey
             )
         } catch {
             throw AccountProviderError.invalidInput
