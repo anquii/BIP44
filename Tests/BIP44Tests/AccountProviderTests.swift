afaf2ba7eb7c61847a7e3364e9f4ff44bac3f49e
anquii <50277099+anquii@users.noreply.github.com>
2022-04-11 18:36:03 +0100

    Added AccountProviderTests

---
+++ Tests/BIP44Tests/AccountProviderTests.swift
@@ -0,0 +1,24 @@
+import XCTest
+import BIP32
+@testable import BIP44
+
+final class AccountProviderTests: XCTestCase {
+    private let serializedKeyCoder = SerializedKeyCoder()
+
+    private func sut(seed: Data, coinType: CoinType) throws -> AccountProvider {
+        try .init(seed: seed, coinType: coinType)
+    }
+
+    func testGivenVectorSeed_AndCoinType_WhenProvideAccount_ThenEqualVectorAccount() throws {
+        let seed = Data(hex: AccountTestVector.hexEncodedSeed)
+        let sut = try self.sut(seed: seed, coinType: Bitcoin())
+        let configuration = AccountConfiguration(
+            name: AccountTestVector.name,
+            version: AccountTestVector.version,
+            index: AccountTestVector.index
+        )
+        let account = try sut.account(configuration: configuration)
+        let base58CheckEncodedPrivateKey = serializedKeyCoder.encode(serializedKey: account.serializedKey)
+        XCTAssertEqual(base58CheckEncodedPrivateKey, AccountTestVector.base58CheckEncodedPrivateKey)
+    }
+}
