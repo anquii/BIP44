e0c71777ab5d2c3504339031c1b58203d788737e
anquii <50277099+anquii@users.noreply.github.com>
2022-04-11 17:33:26 +0100

    Added AccountConfiguration

---
+++ Sources/BIP44/AccountConfiguration.swift
@@ -0,0 +1,15 @@
+public struct AccountConfiguration {
+    public let name: String
+    public let version: UInt32
+    public let index: UInt32
+
+    public init(
+        name: String,
+        version: UInt32,
+        index: UInt32
+    ) {
+        self.name = name
+        self.version = version
+        self.index = index
+    }
+}
