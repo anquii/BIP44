f2867bb4437958c5a6a526ca18906ac6dfe9f723
anquii <50277099+anquii@users.noreply.github.com>
2022-04-18 10:20:12 +0100

    Added NeuteredAccountImporter

---
+++ Sources/BIP44/NeuteredAccountImporter.swift
@@ -0,0 +1,30 @@
+import BIP32
+
+public protocol NeuteredAccountImporting {
+    func neuteredAccount(name: String, coinType: CoinType, base58CheckEncodedPublicKey: String) throws -> NeuteredAccount
+}
+
+public struct NeuteredAccountImporter {
+    private static let depth = UInt8(3)
+
+    private let serializedKeyDecoder: SerializedKeyDecoding
+
+    public init(serializedKeyDecoder: SerializedKeyDecoding = SerializedKeyCoder()) {
+        self.serializedKeyDecoder = serializedKeyDecoder
+    }
+}
+
+// MARK: - NeuteredAccountImporting
+extension NeuteredAccountImporter: NeuteredAccountImporting {
+    public func neuteredAccount(name: String, coinType: CoinType, base58CheckEncodedPublicKey: String) throws -> NeuteredAccount {
+        do {
+            let serializedKey = try serializedKeyDecoder.decode(string: base58CheckEncodedPublicKey)
+            guard serializedKey.depth == Self.depth else {
+                throw AccountImporterError.invalidAccount
+            }
+            return NeuteredAccount(name: name, coinType: coinType, serializedKey: serializedKey)
+        } catch {
+            throw AccountImporterError.invalidInput
+        }
+    }
+}
