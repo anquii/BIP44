d8dd5a4fcdbfeadd37583b7f58dbda1c868c4d7f
anquii <50277099+anquii@users.noreply.github.com>
2023-12-02 18:24:59 +0000

    Updated dependency graph

--- Package.swift
+++ Package.swift
@@ -9,32 +9,14 @@ let package = Package(
         .iOS(.v13)
     ],
     products: [
-        .library(
-            name: "BIP44",
-            targets: ["BIP44"]
-        )
+        .library(name: "BIP44", targets: ["BIP44"])
     ],
     dependencies: [
-        .package(
-            url: "https://github.com/anquii/BIP32.git",
-            .exact("1.1.1")
-        ),
-        .package(
-            url: "https://github.com/anquii/CryptoSwiftWrapper.git",
-            .exact("1.4.3")
-        )
+        .package(url: "https://github.com/anquii/BIP32.git", .exact("1.1.2")),
+        .package(url: "https://github.com/anquii/CryptoSwiftWrapper.git", .exact("1.4.3"))
     ],
     targets: [
-        .target(
-            name: "BIP44",
-            dependencies: ["BIP32"]
-        ),
-        .testTarget(
-            name: "BIP44Tests",
-            dependencies: [
-                "BIP44",
-                "CryptoSwiftWrapper"
-            ]
-        )
+        .target(name: "BIP44", dependencies: ["BIP32"]),
+        .testTarget(name: "BIP44Tests", dependencies: ["BIP44", "CryptoSwiftWrapper"])
     ]
 )
